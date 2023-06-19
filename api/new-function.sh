67b949ef2097e880ec9461f25fc78a76f60e65b3
lvirrueta <luis510vg@gmail.com>
2023-06-19 01:31:31 -0600

    new function script

---
+++ api/new-function.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+newF=$1
+
+nest g mo $newF
+
+mkdir src/$newF/application
+nest g cl $newF/application/dto/create-$newF.dto --flat --no-spec
+nest g cl $newF/application/dto/update-$newF.dto --flat --no-spec
+mkdir src/$newF/application/controller
+nest g co $newF/application/controller/$newF --flat --no-spec
+mkdir src/$newF/application/model
+nest g cl $newF/application/model/$newF-api --flat --no-spec
+
+mkdir src/$newF/domain
+mkdir src/$newF/domain/service
+nest g s $newF/domain/service/$newF --flat --no-spec
+mkdir src/$newF/domain/ientities
+nest g itf $newF/domain/ientities/i-$newF --flat
+mkdir src/$newF/domain/irepositories
+nest g itf $newF/domain/irepositories/i-$newF.repository --flat
+
+mkdir src/$newF/infrastructure
+mkdir src/$newF/infrastructure/entities
+nest g cl $newF/infrastructure/entities/$newF.entity --flat --no-spec
+mkdir src/$newF/infrastructure/repositories
+nest g cl $newF/infrastructure/repositories/$newF.repository --flat --no-spec
