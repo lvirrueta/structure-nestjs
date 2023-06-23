724918d93bc694ec3dad9c4a799e8f5b5a1b0c89
lvirrueta <luis510vg@gmail.com>
2023-06-23 16:10:16 -0600

    add info to swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,18 +7,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const options = new DocumentBuilder()
-    .setTitle('')
-    .setDescription('')
-    .setVersion('')
-    // .addTag('')
-    // .addBearerAuth()
-    //.addSecurity('basic', {type: 'http',scheme: 'basic',})
+    .setTitle('Beer Pub')
+    .setDescription('App Beer Pub')
+    .setVersion('0.0.1')
+    .addBearerAuth()
+    .addSecurityRequirements('bearer')
+    //.addSecurity('basic', {type: 'http',scheme: 'basic'})
     //.addOAuth2()
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document, {
     swaggerOptions: {
       filter: true,
+      persistAuthorization: true,
     },
   });
 
