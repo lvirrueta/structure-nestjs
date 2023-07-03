e85875da6db4704fb5e01cc994eb9b09c842fa0f
lvirrueta <luis510vg@gmail.com>
2023-07-03 15:57:49 -0600

    user repository: rearrange imports

--- api/src/sso/infrastructure/repositories/user/user.repository.ts
+++ api/src/sso/infrastructure/repositories/user/user.repository.ts
@@ -1,15 +1,24 @@
+// Dependencies
+import { DataSource } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+
+// Model
+import { UserEntity } from '../../entities/user/user.entity';
+import { IUser, User } from 'src/sso/domain/models/user/user.model';
+
 // IRepository
 import { IUserRepository } from 'src/sso/domain/irepositories/user/i-user.repository.interface';
 
 // Repository
 import { GenericRepository } from 'src/common/infrastructure/repositories/generic.repository';
-import { IUser, User } from 'src/sso/domain/models/user/user.model';
-import { Injectable } from '@nestjs/common';
-import { DataSource } from 'typeorm';
-import { UserEntity } from '../../entities/user/user.entity';
 
 /** UserRepository */
 export class UserBaseRepository<M extends User, I extends IUser> extends GenericRepository<M> implements IUserRepository<M, I> {
+  relations(): (object: M) => any {
+    return (r) => [r.userInfo];
+  }
+
+  /** find By Username */
   public async findByUsername(username: string): Promise<M> {
     const qb = this.createQueryBuilder('user');
     qb.select(['user.username', 'user.password', 'user.id']);
