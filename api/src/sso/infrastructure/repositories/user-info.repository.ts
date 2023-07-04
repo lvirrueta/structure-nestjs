521d705805df3b72c74f365657b528589b960e2b
lvirrueta <luis510vg@gmail.com>
2023-07-03 18:30:13 -0600

    user info: lower the email before insert

--- api/src/sso/infrastructure/repositories/user-info.repository.ts
+++ api/src/sso/infrastructure/repositories/user-info.repository.ts
@@ -4,8 +4,9 @@ import { Injectable } from '@nestjs/common';
 import { GenericRepository } from 'src/common/infrastructure/repositories/generic.repository';
 import { IUserInfoRepository } from 'src/sso/domain/irepositories/user-info.repository.interface';
 
-import { UserInfo } from 'src/sso/domain/models/user-info.model';
+import { IUserInfo, UserInfo } from 'src/sso/domain/models/user-info.model';
 import { UserInfoEntity } from '../entities/user-info.entity';
+import { IRepositoryOpt } from 'src/common/domain/irepositories/i-repository.repository.interface';
 
 @Injectable()
 export class UserInfoRepository extends GenericRepository<UserInfo> implements IUserInfoRepository {
@@ -15,5 +16,15 @@ export class UserInfoRepository extends GenericRepository<UserInfo> implements I
 
   relations(): (object: UserInfo) => any {
     return () => [];
+  }
+
+  public async createEntity(dto: IUserInfo, opt?: IRepositoryOpt): Promise<UserInfo> {
+    dto.email = dto.email.toLowerCase();
+    return await super.createEntity(dto, opt);
+  }
+
+  public async updateEntity(dto: IUserInfo, opt?: IRepositoryOpt): Promise<UserInfo> {
+    dto.email = dto.email.toLowerCase();
+    return await super.updateEntity(dto, opt);
   }
 }
