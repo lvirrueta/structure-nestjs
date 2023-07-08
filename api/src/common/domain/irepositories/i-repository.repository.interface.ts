840e24040996e2aa4a41256d853a69186ae27032
lvirrueta <luis510vg@gmail.com>
2023-07-07 20:30:19 -0600

    add interface without transactions

--- api/src/common/domain/irepositories/i-repository.repository.interface.ts
+++ api/src/common/domain/irepositories/i-repository.repository.interface.ts
@@ -1,13 +1,10 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
 import { QueryRunner } from 'typeorm';
 
 import { Search } from 'src/common/application/dto/search.dto';
 import { ID } from 'src/common/application/types/types.types';
 
-/**
- * @param M Model Entity
- * @param I Interface Entity
- */
-export interface IGenericRepository<E = any, IE = any> {
+interface IMethodsGenericRepository<E = any, IE = any> {
   listEntities(search?: Search, opt?: IRepositoryOpt): Promise<E[]>;
   listEntitiesAndCount(search?: Search, opt?: IRepositoryOpt): Promise<[E[], number]>;
   findOneEntity(id: ID, opt?: IRepositoryOpt): Promise<E>;
@@ -17,11 +14,28 @@ export interface IGenericRepository<E = any, IE = any> {
   updateEntities(entity: IE[], opt?: IRepositoryOpt): Promise<E[]>;
   deleteEntity(id: ID, opt?: IRepositoryOpt): Promise<E>;
   softDeleteEntity(id: ID, opt?: IRepositoryOpt): Promise<E>;
+}
+
+interface ITransactionGenericRepository {
   createAndStartTransaction(): Promise<QueryRunner>;
   commitTransaction(query: QueryRunner): Promise<void>;
   rollbackTransaction(query: QueryRunner): Promise<void>;
   releaseTransaction(query: QueryRunner): Promise<void>;
 }
+
+/**
+ * Interface of generic repository with transactions
+ * @param M Model Entity
+ * @param I Interface Entity
+ */
+export interface IGenericRepository<E = any, IE = any> extends IMethodsGenericRepository<E, IE>, ITransactionGenericRepository {}
+
+/**
+ * Interface of generic repository without transactions
+ * @param M Model Entity
+ * @param I Interface Entity
+ */
+export interface IGenericRepositoryNTr<E = any, IE = any> extends IMethodsGenericRepository<E, IE> {}
 
 export interface IRepositoryOpt<T = any> {
   /**
